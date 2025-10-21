8014a463c3999681d93f956d8af322b3b90ea79a
Oliver Annus <oliver.annus@gmail.com>
2025-10-21 21:09:28 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'my-vite-app', // Replace <your-repo-name> with your actual GitHub repository name
+  base: '/my-vite-app/', // Replace <your-repo-name> with your actual GitHub repository name
 });
